11d9418b8dc83f44a845cdd1c3503f0eed1bf16f
Chirag Narkar <chiragnarkar2507@gmail.com>
2025-11-04 23:48:17 -0600

    Auto backup on Tue 11/04/2025 23:48:17.75

--- auto_push.bat
+++ auto_push.bat
@@ -1,5 +1,11 @@
 @echo off
 cd "C:\GMU\Summer 2024\Dashboard\dashboard"
-git add .
-git commit -m "Auto backup on %date% %time%"
-git push origin main
+
+echo ==== Starting Auto Push at %date% %time% ==== >> autopush_log.txt
+
+"C:\Program Files\Git\bin\git.exe" pull origin main --rebase >> autopush_log.txt 2>&1
+"C:\Program Files\Git\bin\git.exe" add . >> autopush_log.txt 2>&1
+"C:\Program Files\Git\bin\git.exe" commit -m "Auto backup on %date% %time%" >> autopush_log.txt 2>&1
+"C:\Program Files\Git\bin\git.exe" push origin main >> autopush_log.txt 2>&1
+
+echo ==== Completed at %date% %time% ==== >> autopush_log.txt
